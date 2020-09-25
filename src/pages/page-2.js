d2d9afccfaf88b4496b3e5d054eff16bac2dfcef
Cathy Geddes <catgeddes@icloud.com>
2020-09-25 13:50:33 +1200

    form

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,11 +7,28 @@ import Navigation from "../components/nav"
 
 const SecondPage = () => (
   <Layout>
-   <Navigation></Navigation>
+    <Navigation></Navigation>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+
+    <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+    <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   </Layout>
-)
+
+);
 
 export default SecondPage
